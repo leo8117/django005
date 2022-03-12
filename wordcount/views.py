6bd27e042d4b8bd75d4e97868cbe2588f6efaa7d
Leo C <8117leo@gmail.com>
2022-03-12 18:35:56 -0500

    wordcount

---
+++ wordcount/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+import operator
+
+def home(request):
+    return render(request, "word.html", {"conttext":"100"} )
+
+def countword(request):
+    words = request.GET['fulltext']
+    wordlist=words.split(" ")
+    worddict= {}
+    for word in wordlist:
+        if word in worddict:
+            worddict[word] += 1
+        else:
+            worddict[word] = 1
+    sortedword = sorted(worddict.items(), key=operator.itemgetter(1), reverse=True)
+    print(wordlist)
+    print(sortedword)
+
+    return render(request, 'count.html', {"words":words, "len":len(wordlist), "worddict": worddict })
